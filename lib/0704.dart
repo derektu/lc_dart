93ef1a741ca920003f0a764629edb636c66afa15
derektu <derektu123@gmail.com>
2022-11-08 11:42:33 +0800

    704 AC

--- lib/0704.dart
+++ lib/0704.dart
@@ -1,5 +1,24 @@
+///
+/// BinarySearch, find exact match of [target] inside [nums] array.
+/// [left] and [right] are INCLUSIVE
+/// return the found index, of -1 if not found
+///
+int bsearch(List<int> nums, int left, int right, int target) {
+  while (left <= right) {
+    var mid = left + (right - left) ~/ 2;
+    if (nums[mid] == target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else /*if (nums[mid] > target)*/ {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
 class Solution {
   int search(List<int> nums, int target) {
-    throw 'TODO';
+    return bsearch(nums, 0, nums.length - 1, target);
   }
 }
